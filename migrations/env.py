19f5a7f9e2b2fe0aceff74c4b2a2e2fd07628dfe
martin76ec <larreaamartin@gmail.com>
2025-03-06 09:53:14 -0500

    feat: add poe tasks to manage migrations fix: remove init from models fix: replace models import

--- migrations/env.py
+++ migrations/env.py
@@ -1,39 +1,42 @@
 from logging.config import fileConfig
 from sqlalchemy import create_engine
 from alembic import context
-from src.providers.sqlalchemy.models import Reminders  # Ensure all models are imported here if needed
+from src.providers.sqlalchemy.models.models import Reminders  # Ensure all models are imported here if needed
 
 config = context.config
 
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+  fileConfig(config.config_file_name)
 
 engine = create_engine("sqlite:///life-assistant.db")
 
 # Use the metadata defined on your Reminders model (and any others sharing the same base)
 target_metadata = Reminders.metadata
 
+
 def run_migrations_offline() -> None:
-    context.configure(
-        url="sqlite:///life-assistant.db",
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-    with context.begin_transaction():
-        context.run_migrations()
+  context.configure(
+    url="sqlite:///life-assistant.db",
+    target_metadata=target_metadata,
+    literal_binds=True,
+    dialect_opts={"paramstyle": "named"},
+  )
+  with context.begin_transaction():
+    context.run_migrations()
+
 
 def run_migrations_online() -> None:
-    with engine.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            compare_type=True,
-        )
-        with context.begin_transaction():
-            context.run_migrations()
+  with engine.connect() as connection:
+    context.configure(
+      connection=connection,
+      target_metadata=target_metadata,
+      compare_type=True,
+    )
+    with context.begin_transaction():
+      context.run_migrations()
+
 
 if context.is_offline_mode():
-    run_migrations_offline()
+  run_migrations_offline()
 else:
-    run_migrations_online()
+  run_migrations_online()
--- src/providers/sqlalchemy/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .models import Reminders
-from .models import SpendingGroup
-from .models import Spendings
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
