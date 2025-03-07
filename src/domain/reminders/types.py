3393bc1b93c0ed7d6fb46a5e43ab73d13452fb05
martin76ec <larreaamartin@gmail.com>
2025-03-07 11:53:00 -0500

    feat: add reminder data type

---
+++ src/domain/reminders/types.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class ReminderModel(BaseModel):
+  message: str
+  reminder_date: str
+  status: Optional[str]
+  recurrence: Optional[str]
