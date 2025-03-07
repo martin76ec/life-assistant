24a5a50c3e821850e6a0dd1eea9262e7228f878d
martin76ec <larreaamartin@gmail.com>
2025-03-07 11:53:34 -0500

    feat: fix session dependencies and reminder instance

---
+++ src/infrastructure/database/reminders/reminders_ds.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from sqlalchemy.ext.asyncio import AsyncSession
+from domain.reminders.types import ReminderModel
+from providers.sqlalchemy.models.dataclasses import Reminders
+
+
+class RemindersDS:
+  @staticmethod
+  async def create_reminder(reminder: ReminderModel, session: AsyncSession):
+    """Creates a new reminder."""
+    data = Reminders(
+      id=None,
+      message=reminder.message,
+      recurrence=reminder.recurrence,
+      reminder_date=datetime.strptime(reminder.reminder_date, "%Y-%m-%d %H:%M"),
+      created_at=None,
+      updated_at=None,
+      status=None,
+      is_deleted=None,
+    )
+    session.add(data)
+    await session.commit()
+    await session.refresh(data)
+    return data
