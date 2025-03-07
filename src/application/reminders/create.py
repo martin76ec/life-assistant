54125da851781dbbbe1259a141f0c98cb19171ef
martin76ec <larreaamartin@gmail.com>
2025-03-07 11:54:07 -0500

    fix: add missing router decorator fix: add session instance

--- src/application/reminders/create.py
+++ src/application/reminders/create.py
@@ -1,23 +1,23 @@
-# todo: create a function that imports Reminders model from src.providers.sqlalchemy.models.models and creates a new row
-# context: if try except needed use a general try except in the main thread
-# context: define dataclasses for params if needed
-# context: for function naming always use entity_action template
-# context: don't put unncessary comments
-
 from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from domain.reminders.types import ReminderModel
+from providers.sqlalchemy.engine import EngineSingleton
 from src.providers.sqlalchemy.models.models import Reminders
+from src.infrastructure.database.reminders.reminders_ds import RemindersDS
 
 
-async def create_reminder(reminder: Reminders, session: AsyncSession) -> Optional[Reminders]:
-  """Creates a new reminder."""
-  try:
-    session.add(reminder)
-    await session.commit()
-    await session.refresh(reminder)
-    return reminder
-  except Exception as e:
-    print(f"Error creating reminder: {e}")
-    await session.rollback()
-    return None
+class Create:
+  @staticmethod
+  async def run(reminder: dict) -> Optional[Reminders]:
+    """Creates a new reminder."""
+    engine = EngineSingleton.instance_get()
+    session = AsyncSession(engine)
+
+    try:
+      valid_reminder = ReminderModel.model_validate(reminder, strict=True)
+      await RemindersDS.create_reminder(valid_reminder, session)
+    except Exception as e:
+      print(f"Error creating reminder: {e}")
+      await session.rollback()
+    finally:
+      await session.close()
--- src/application/reminders/router.py
+++ src/application/reminders/router.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
 
+from application.reminders.create import Create
+
 router = APIRouter()
 
 
 @router.get("/reminders")
 async def reminders_get():
   return {"message": "List of items"}
+
+
+@router.post("/")
+async def reminders_create(data: dict):
+  await Create.run(data)
